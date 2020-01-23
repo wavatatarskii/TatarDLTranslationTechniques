e95f0892557d56b236fac12998a8a2f810ccf900
Oleg <34554388+wavatatarskii@users.noreply.github.com>
2020-01-23 21:46:46 +0300

    Update Import module.py

--- Model/Import module.py
+++ Model/Import module.py
@@ -14,3 +14,39 @@ tf.compat.v1.enable_eager_execution()
 # Set to 'infer' will skip the training
 MODE = 'train'
 FILENAME = '/content/TatarDLTranslationTechniques/tatTABru60000-1.zip'
+
+def maybe_download_and_read_file(url, filename):
+    """ Download and unzip training data
+    Args:
+        url: data url
+        filename: zip filename
+    
+    Returns:
+        Training data: an array containing text lines from the data
+    """
+    if not os.path.exists(filename):
+        session = requests.Session()
+        response = session.get(url, stream=True)
+
+        CHUNK_SIZE = 32768
+        with open(filename, "wb") as f:
+            for chunk in response.iter_content(CHUNK_SIZE):
+                if chunk:
+                    f.write(chunk)
+
+    zipf = ZipFile(filename)
+    filename = zipf.namelist()
+    with zipf.open(filename[0]) as f:
+        lines = f.read()
+
+    return lines
+
+
+
+
+
+
+def normalize_string(s):
+    s = re.sub(r'([!.?])', r' \1', s)
+    s = re.sub(r'\s+', r' ', s)
+    return s
