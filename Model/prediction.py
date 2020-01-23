d862fe28a6747b584bf9ce492457179a7acf7399
Oleg <34554388+wavatatarskii@users.noreply.github.com>
2020-01-23 21:53:49 +0300

    Create prediction.py

---
+++ Model/prediction.py
@@ -0,0 +1,40 @@
+def predict(test_source_text=None):
+    """ Predict the output sentence for a given input sentence
+    Args:
+        test_source_text: input sentence (raw string)
+    
+    Returns:
+        The encoder's attention vectors
+        The decoder's bottom attention vectors
+        The decoder's middle attention vectors
+        The input string array (input sentence split by ' ')
+        The output string array
+    """
+    if test_source_text is None:
+        test_source_text = raw_data_tt[np.random.choice(len(raw_data_tt))]
+    print(test_source_text)
+    test_source_seq = tt_tokenizer.texts_to_sequences([test_source_text])
+    print(test_source_seq)
+
+    en_output, en_alignments = encoder(tf.constant(test_source_seq), training=False)
+
+    de_input = tf.constant(
+        [[ru_tokenizer.word_index['<start>']]], dtype=tf.int64)
+
+    out_words = []
+
+    while True:
+        de_output, de_bot_alignments, de_mid_alignments = decoder(de_input, en_output, training=False)
+        new_word = tf.expand_dims(tf.argmax(de_output, -1)[:, -1], axis=1)
+        out_words.append(ru_tokenizer.index_word[new_word.numpy()[0][0]])
+
+        # Transformer doesn't have sequential mechanism (i.e. states)
+        # so we have to add the last predicted word to create a new input sequence
+        de_input = tf.concat((de_input, new_word), axis=-1)
+
+        # TODO: get a nicer constraint for the sequence length!
+        if out_words[-1] == '<end>' or len(out_words) >= 14:
+            break
+
+    print(' '.join(out_words))
+    return en_alignments, de_bot_alignments, de_mid_alignments, test_source_text.split(' '), out_words
