7c346ad5f6f67e07042b990223000ee78139ee5f
Oleg <34554388+wavatatarskii@users.noreply.github.com>
2020-01-23 21:37:48 +0300

    Translator interface class uploaded

---
+++ Lang Data Handler/Translator/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Translator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(9, 10);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 123);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Paste from clipboard";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(157, 10);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(110, 123);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Copy from clipboard";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(71, 215);
+            this.button3.Margin = new System.Windows.Forms.Padding(2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(124, 45);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Push it";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 164);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(10, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = " ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(68, 164);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(10, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = " ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(278, 290);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Data Handler";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Lang Data Handler/Translator/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO; //для файлов
+using System.Reflection; //для получения пути
+
+namespace Translator
+{
+    public partial class Form1 : Form
+    {
+        string path;//путь к дирректории
+        string[] Mass;//массив из файла
+        int length;//тут хранимся вся длина массива
+        int currentstring=71139;//текущая позиция в массиве 
+        string m = "";//буфер обмена с татарским текстом
+        public Form1()
+        {
+            InitializeComponent();
+
+            //получение директории где находится exe
+            path = (Assembly.GetExecutingAssembly().Location);              //получение пути файла
+            {//ограничение зоны видимости
+                string[] s2 = path.Split('\\');
+                path = path.TrimEnd(s2[s2.Length - 1].ToCharArray());       //удаление имени и расширения файла из пути
+            }
+            //чтение из файла
+            Mass = File.ReadAllLines($"{path}\\input.txt", System.Text.Encoding.Unicode);//у нас Unicode , а не ACSII
+            length = Mass.Length;//кол-во строк в массиве
+            label2.Text = length.ToString();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {   //тут вставка в буфер обмена
+            
+            if (currentstring == length) { Application.Exit(); }//чтобы вышел, когда всё прочитает
+            int charamount=0;
+            int QuantityOfStrings = 0;//кол-во строк в массиве для буфера обмена
+            //currentstring глобальная
+            //length = Mass.Length глобальная
+            int previouspisition = currentstring;
+            
+            for (; currentstring < length; currentstring++)// считаем кол-во строк нужных
+            {
+                QuantityOfStrings++;
+                charamount += Mass[currentstring].Length;
+                if (charamount >= 800||(currentstring-previouspisition)>6)
+                {
+                    charamount -= Mass[currentstring].Length;
+                    QuantityOfStrings--;
+                    break;
+                }
+            }
+            //тут заносим в массив строк нужные строки
+            m="";//тут будут хранится все нужные строки 
+
+            for (int i = QuantityOfStrings; i > 0; i--)
+            {
+                m = m +'\n'+ Mass[previouspisition + (QuantityOfStrings - i)];//правильный порядок
+            }
+
+            Clipboard.Clear();
+            Clipboard.SetText(m);
+            button1.Enabled = false;
+            button2.Enabled = true;
+            label1.Text = currentstring.ToString();
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {//тут копирование из буфера обмена в файл
+            string a = Clipboard.GetText();
+            char[] ms = a.ToCharArray();
+            int erroror = 0;
+            //for (int i = 0; i < ms.Length && erroror == 0; i++)
+            //{
+            //    if (ms[i] == 'Ә' || ms[i] == 'ә' || ms[i] == 'Ө' || ms[i] == 'ө'
+            //        || ms[i] == 'Ү' || ms[i] == 'ү' || ms[i] == 'Җ' || ms[i] == 'җ'
+            //        || ms[i] == 'Ң' || ms[i] == 'ң' || ms[i] == 'Һ' || ms[i] == 'һ') erroror++;
+            //}
+            if (erroror > 0) Clipboard.SetText(m);
+            else
+            {
+                //////////////////добавляем после .!? символ l///////////////////////////////////////
+                {//ограничение зоны видимости
+
+                    for (int j = a.Length - 1; j >= 0; j--)
+                    {
+                        //проверка для троеточий (3 это когда один символ и ... после него) вдруг такое может быть
+                        //первое условие для одинарных символов, второе для ...
+                        if (j == a.Length - 1||j==2||j==1||j==0)//если последний элемент вопрос
+                        {
+                            if (ms[j] == '?') a = a.Insert(j + 1, "l");
+                        }
+                        else
+                        {
+                            if (((ms[j] == '.' || ms[j] == '!' || ms[j] == '?') && (ms[j - 1] != '.') && (ms[j + 1] != '.')) || (ms[j] == '.' && ms[j - 1] == '.' && ms[j - 2] == '.')) a = a.Insert(j + 1, "l");//добавление символа l после .!?
+                        }
+
+                    }
+                }
+                //запись в файл
+                string[] mass2 = a.Split('l');//строки для предложений из абзаца
+
+                for (int j = 0; j < mass2.Length; j++)
+                {
+                    Console.WriteLine("Идет запись " + (j + 1) + " из " + mass2.Length);
+
+                    char[] ms2 = mass2[j].ToCharArray();
+                    if (ms2.Length != 0)
+                    {
+                        if (ms2[ms2.Length - 1] != '?' && (ms2[ms2.Length - 1] == '.' || ms2[ms2.Length - 1] == '!'))//отсеиваем предложения с ? и предложения без знака в конце
+                        {
+
+                            using (StreamWriter SW = new StreamWriter(new FileStream($"{path}\\ru.txt", FileMode.Append, FileAccess.Write), Encoding.Unicode))
+                            {
+
+                                SW.WriteLine(mass2[j]);
+                                SW.Flush();
+                            }
+
+                        }
+                    }
+                    //Console.Clear();
+                }
+
+
+                button1.Enabled = true;
+                button2.Enabled = false;
+            }
+        }//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            int quantityofbags = 0;
+            for (int i = 0; i < length; i++)
+            {
+                if (Mass[i].Length >= 800) quantityofbags++;
+                
+            }
+            if (quantityofbags == 0) {
+                button1.Enabled = true;
+                button3.Enabled = false;
+            }
+            else MessageBox.Show("ТУТ ГДЕ ТО ЕСТЬ БОЛЕЕ 800 символов строка");
+
+        }
+    }
+}
